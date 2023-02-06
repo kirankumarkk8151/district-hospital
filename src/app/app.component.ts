737bcd578e9315ed4f4989560522a6dd12b8196b
Kiran <kirankumarkk8151@gmail.com>
2023-02-06 14:46:49 +0530

    inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+import{Database,set,ref,update,onValue} from '@angular/fire/database'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'district_hospital';
+  constructor(private db:Database)
+  {
+
+  }
+  addData(f:any)
+  {
+    set(ref(this.db, 'users/' + f.value.name), {
+      username: f.value.name,
+      email: f.value.email,
+      password:f.value.password
+    });
+  }
+  readData(f:any)
+  {
+    const starCountRef = ref(this.db, 'users/' +  f.value.name);
+onValue(starCountRef, (snapshot) => {
+  const data = snapshot.val();
+  console.log(data.email)
+ 
+});
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment={
+  firebase: {
+    projectId: 'district-hospital-492f5',
+    appId: '1:510601808379:web:69ad572725eb08ba90e35e',
+    storageBucket: 'district-hospital-492f5.appspot.com',
+    apiKey: 'AIzaSyDYoiaq8UpzMHdgjs8SROUcBHefPAsBqQk',
+    authDomain: 'district-hospital-492f5.firebaseapp.com',
+    messagingSenderId: '510601808379',
+  },
+    production:false
+}
